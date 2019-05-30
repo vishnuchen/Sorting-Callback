b5b7753a88066ed3d0a8afa1fe4178e36adb156e
Vishnu Chennuru <vishnuchen@gmail.com>
2019-05-30 19:42:56 +0000

    first commit

---
+++ sorting.js
@@ -0,0 +1,22 @@
+var students = [
+    { id: 1, name: "bruce",    age: 40 },
+    { id: 2, name: "zoidberg", age: 22 },
+    { id: 3, name: "alex",     age: 22 },
+    { id: 4, name: "alex",     age: 30 }
+  ];
+  
+  
+  var list = students.sort(function(a,b){
+    if (a.name > b.name) {
+      return 1;
+    } else if (a.name === b.name) {
+      return b.age - a.age
+    } else {
+      return -1;
+    }
+  })
+  
+  console.log(list);
+  
+  
+  
